71d10a980014c4c9538c8177e74bd43392f3541d
PythicCoder <aribornstein@gmail.com>
2021-06-08 19:11:03 +0300

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 
 import flash
 import pytorch_lightning as pl
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR100
 from pytorch_lightning.callbacks import EarlyStopping
 from flash.core.classification import Labels
 from flash.core.finetuning import FreezeUnfreeze
@@ -19,13 +19,11 @@ from flash.image import ImageClassificationData, ImageClassifier
 
 if __name__ == "__main__":
     
-    image_size = 128
-
-
     parser = ArgumentParser()
+    parser.add_argument('--image_size', type=int, default=224)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
-    parser.add_argument("--backbone", default="resnet18", type=str)
-    parser.add_argument("--es_monitor", default="val_loss", type=str,)
+    parser.add_argument("--backbone", default="vit_small_patch16_224", type=str)
+    parser.add_argument("--es_monitor", default=None, type=str,)
     parser.add_argument("--es_stopping_threshold", default=None, type=float)
     parser.add_argument("--es_divergence_threshold", default=None, type=float)
     parser.add_argument("--es_check_finite",default=False, type=bool)
@@ -42,32 +40,37 @@ if __name__ == "__main__":
         return data
 
     # 1. Load the data
-    train_dataset = CIFAR10(train=True, download=True, root='.')
+    train_dataset = CIFAR100(train=True, download=True, root='.')
 
     datamodule = ImageClassificationData.from_datasets(train_dataset=train_dataset, 
-                                                    train_transform=merge_transforms({"pre_tensor_transform":unpack_torchvision},
-                                                                                        train_default_transforms([image_size, image_size])),
-                                                    val_transform=merge_transforms({"pre_tensor_transform":unpack_torchvision},
-                                                                                        default_transforms([image_size, image_size])),
-                                                    val_split=.1,
-                                                    batch_size=128)
+                                                       train_transform=merge_transforms({"pre_tensor_transform":unpack_torchvision},
+                                                                                        train_default_transforms([args.image_size, args.image_size])),
+                                                       val_transform=merge_transforms({"pre_tensor_transform":unpack_torchvision},
+                                                                                        default_transforms([args.image_size, args.image_size])),
+                                                       val_split=.1,
+                                                       batch_size=128)
 
     # 2. Build the model
-    model = ImageClassifier(backbone=args.backbone, learning_rate=args.learning_rate, num_classes=10, 
+if __name__ == "__main__":
+    model = ImageClassifier(backbone=args.backbone, learning_rate=args.learning_rate, num_classes=100,
                             scheduler=MultiStepLR, scheduler_kwargs={"milestones": [50, 75]}, serializer=Labels())
 
     # 3. Early stopping call back 
-    early_stopping = EarlyStopping(
-      monitor=args.es_monitor,
-      mode=args.es_mode,
-      min_delta=args.es_min_delta,
-      stopping_threshold=args.es_stopping_threshold,
-      divergence_threshold=args.es_divergence_threshold,
-      check_finite=args.es_check_finite
-    )
+    if args.es_monitor:
+        early_stopping = EarlyStopping(
+          monitor=args.es_monitor,
+          mode=args.es_mode,
+          min_delta=args.es_min_delta,
+          stopping_threshold=args.es_stopping_threshold,
+          divergence_threshold=args.es_divergence_threshold,
+          check_finite=args.es_check_finite)
+
 
     # 4. Create the trainer
-    trainer = flash.Trainer.from_argparse_args(args, callbacks=[early_stopping])
+    if args.es_monitor:
+        trainer = flash.Trainer.from_argparse_args(args, callbacks=[early_stopping])
+    else:
+        trainer = flash.Trainer.from_argparse_args(args)
 
     # 5. Train the model
-    trainer.finetune(model, datamodule=datamodule, strategy=FreezeUnfreeze(unfreeze_epoch=7)))
+    trainer.finetune(model, datamodule=datamodule, strategy=FreezeUnfreeze(unfreeze_epoch=10))
